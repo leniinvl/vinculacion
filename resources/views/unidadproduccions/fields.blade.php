119c032d1c8d9531af1ad3ab76861db1cd5c36ed
luismartin9595 <luismartintieso5@gmail.com>
2018-02-14 21:46:39 -0500

    mapa-boton_buscar-puntos_en_mapas

--- resources/views/unidadproduccions/fields.blade.php
+++ resources/views/unidadproduccions/fields.blade.php
@@ -17,6 +17,7 @@
 </div>
 
 <div id="map">mapa</div>
+<input id="pac-input" type="text" placeholder="Search Box">
 
 <!-- Observaciones Field -->
 <div class="form-group col-sm-12 col-lg-12">
@@ -46,41 +47,94 @@
 <script>
     var map;
     var marker;
+    
     function initMap() {
+        
         map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -0.179707, lng: -78.506866},
-        zoom: 17,
-        draggable: true,
-        zoomControl: true,
-        scrollwheel: true,
-        disableDoubleClickZoom: true
+            center: {lat: 0.14309590647516032, lng: -78.80012830863114},
+            zoom: 10,
+            draggable: true,
+            zoomControl: true,
+            scrollwheel: true,
+            disableDoubleClickZoom: true
         });
+
         marker = new google.maps.Marker({
-        map: map
-    });
+            map: map
+        });
 
-    google.maps.event.addListener(map, 'click', function(args) {
-        console.log(args.latLng.lat());
-        $('#lat').val(args.latLng.lat());
-        $('#lng').val(args.latLng.lng());
-        marker.setPosition(args.latLng);
-    });
+        google.maps.event.addListener(map, 'click', function(args) {
+            console.log(args.latLng.lat());
+            $('#lat').val(args.latLng.lat());
+            $('#lng').val(args.latLng.lng());
+            marker.setPosition(args.latLng);
+        });
 
-    // Construct the polygon.
-    var bermudaTriangle = new google.maps.Polygon({
-        paths: triangleCoords,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: '#FF0000',
-        fillOpacity: 0.35,
-        clickable: false
-    });
-    bermudaTriangle.setMap(map);
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        //input.value="prueba";
+        var searchBox = new google.maps.places.SearchBox(input);
+        //alert(input.value);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+        });
     }
 </script>
 
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTIGv1Whkt5IaGpQe6iifIx3RKz798tr8&callback=initMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTIGv1Whkt5IaGpQe6iifIx3RKz798tr8&libraries=places&callback=initMap" async defer></script>
 @endsection
 
 
@@ -91,6 +145,24 @@
     width: 100%;
     height: 300px;
 }
+
+#pac-input {
+    background-color: #fff;
+    font-family: Roboto;
+    font-size: 15px;
+    font-weight: 300;
+    margin-left: 12px;
+    margin-top: 14px;
+    padding: 0 11px 0 13px;
+    text-overflow: ellipsis;
+    width: 300px;
+}
+
+#pac-input:focus {
+    border-color: #4d90fe;
+}
+
+
 </style>
 
 @endsection
--- resources/views/unidadproduccions/index.blade.php
+++ resources/views/unidadproduccions/index.blade.php
@@ -21,9 +21,78 @@
         <div class="text-center">
         
         </div>
-
-        
-
+        <div id="map_index">mapa</div>
     </div>
 @endsection
 
+@section('scripts')
+<script>
+    var map;
+    var marker;
+    var coords_lat = new Array();
+    var coords_lng = new Array();
+    var nombres = new Array();
+    var cont_lat=0;
+    var cont_lng=0, cont_nomb=0;
+    
+    var datos = document.getElementById('unidadproduccions-table').getElementsByTagName('td');
+    for(var i=0;i<datos.length;i++){
+        //console.log(datos[i]);
+        if(((i-1)%7)==0){
+            coords_lat[cont_lat]=datos[i].innerHTML;
+            cont_lat++;
+        }else{
+            if(((i-2)%7)==0){
+                coords_lng[cont_lng]=datos[i].innerHTML;
+                cont_lng++;
+            }else{
+                if(((i)%7)==0){
+                    nombres[cont_nomb]=datos[i].innerHTML;
+                    cont_nomb++;
+                }
+            }
+        }
+    }
+
+    function initMap() {
+        
+        map = new google.maps.Map(document.getElementById('map_index'), {
+            center: {lat: 0.14309590647516032, lng: -78.80012830863114},
+            zoom: 10,
+            draggable: true,
+            zoomControl: true,
+            scrollwheel: true,
+            disableDoubleClickZoom: true
+        });       
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+        
+        for (i = 0; i < coords_lng.length; i++) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(coords_lat[i], coords_lng[i]),
+                map: map
+            });
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+              return function() {
+                infowindow.setContent(nombres[i]);
+                infowindow.open(map, marker);
+              }
+            })(marker, i));
+        }
+    }
+
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBTIGv1Whkt5IaGpQe6iifIx3RKz798tr8&callback=initMap"></script>
+@endsection
+
+
+@section('css')
+<style type="text/css">
+#map_index {
+    width: 100%;
+    height: 300px;
+}
+</style>
+@endsection
