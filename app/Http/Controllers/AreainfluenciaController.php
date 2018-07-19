a58bfacb12d57dde9788350e6c053b2988dd21b9
waltur87 <turi.vergara@gmail.com>
2018-07-19 13:50:14 -0500

    cambios en areaInfluencia

--- app/Http/Controllers/AreainfluenciaController.php
+++ app/Http/Controllers/AreainfluenciaController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Http\Requests\CreateAreaInfluenciaRequest;
 use App\Http\Requests\UpdateAreaInfluenciaRequest;
 use App\Repositories\AreaInfluenciaRepository;
@@ -11,155 +9,203 @@ use Flash;
 use Prettus\Repository\Criteria\RequestCriteria;
 use Response;
 use Barryvdh\DomPDF\Facade as PDF;
+use App\Models\CondicionesDrenaje;
+use App\Models\TipoVegetal;
+use App\Models\Religion;
+use App\Models\Lenguaje;
+use App\Models\Areainfluencia;
+use App\Models\ManejoAmbiental;
+use App\Models\PermeabilidadSuelo;
+use App\Models\UsoSuelo;
+use App\Models\CaracteristicasEtnicas;
+use App\Models\TipoSuelo;
+use App\Models\Clima;
+use App\Models\Ecosistema;
+
+
 class AreaInfluenciaController extends AppBaseController
 {
-    /** @var  AreaInfluenciaRepository */
-    private $areaInfluenciaRepository;
+  /** @var  AreaInfluenciaRepository */
+  private $areaInfluenciaRepository;
+  public function __construct(AreaInfluenciaRepository $areaInfluenciaRepo)
+  {
+    $this->areaInfluenciaRepository = $areaInfluenciaRepo;
+  }
+  /**
+  * Display a listing of the AreaInfluencia.
+  *
+  * @param Request $request
+  * @return Response
+  */
+  public function index(Request $request)
+  {
+    $this->areaInfluenciaRepository->pushCriteria(new RequestCriteria($request));
+    $areaInfluencias = $this->areaInfluenciaRepository->all();
+    return view('area_influencias.index')
+    ->with('areaInfluencias', $areaInfluencias);
+  }
+  /**
+  * Show the form for creating a new AreaInfluencia.
+  *
+  * @return Response
+  */
+  public function create()
+  {
+    $condicionesdrenaje=CondicionesDrenaje::all()->pluck('nombre','id');
+    $manejoambiental=ManejoAmbiental::all()->pluck('nombre','id');
+    $permeabilidadsuelo=PermeabilidadSuelo::all()->pluck('nombre','id');
+    $usosuelo=UsoSuelo::all()->pluck('nombre','id');
+    $caracteristicasetnicas=CaracteristicasEtnicas::all()->pluck('nombre','id');
+    $tiposuelo=TipoSuelo::all()->pluck('nombre','id');
+    $clima=Clima::all()->pluck('nombre','id');
+    $ecosistema=Ecosistema::all()->pluck('nombre','id');
+    return view('area_influencias.create')->with('condicionesdrenaje', $condicionesdrenaje)->with('permeabilidadsuelo', $permeabilidadsuelo)->with('usosuelo', $usosuelo)->with('caracteristicasetnicas', $caracteristicasetnicas)  ->with('tiposuelo', $tiposuelo)->with('clima', $clima)->with('ecosistema', $ecosistema)->with('manejoambiental', $manejoambiental);
+  }
+  /**
+  * Store a newly created AreaInfluencia in storage.
+  *
+  * @param CreateAreaInfluenciaRequest $request
+  *
+  * @return Response
+  */
+  public function store(CreateAreaInfluenciaRequest $request)
+  {
+    $input = $request->all();
+    $areaInfluencia = $this->areaInfluenciaRepository->create($input);
+    Flash::success('Area Influencia saved successfully.');
+    return redirect(route('areaInfluencias.index'));
+  }
+  /**
+  * Display the specified AreaInfluencia.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function show($id)
+  {
+    $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
+    $tipovegetal = TipoVegetal::all()->pluck('nombre_comun', 'id');
+    $religion = Religion::all()->pluck('nombre', 'id');
+    $lenguaje = Lenguaje::all()->pluck('nombre', 'id');
+    if (empty($areaInfluencia)) {
+      Flash::error('Area Influencia not found');
+      return redirect(route('areaInfluencias.index'));
+    }
+    return view('area_influencias.show')->with('areaInfluencia', $areaInfluencia)->with('tipovegetal', $tipovegetal)->with('religion', $religion)
+    ->with('lenguaje', $lenguaje);
+  }
+  /**
+  * Show the form for editing the specified AreaInfluencia.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function edit($id)
+  {
+    $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
+    $condicionesdrenaje=CondicionesDrenaje::all()->pluck('nombre','id');
+    $manejoambiental=ManejoAmbiental::all()->pluck('nombre','id');
+    $permeabilidadsuelo=PermeabilidadSuelo::all()->pluck('nombre','id');
+    $usosuelo=UsoSuelo::all()->pluck('nombre','id');
+    $caracteristicasetnicas=CaracteristicasEtnicas::all()->pluck('nombre','id');
+    $tiposuelo=TipoSuelo::all()->pluck('nombre','id');
+    $clima=Clima::all()->pluck('nombre','id');
+    $ecosistema=Ecosistema::all()->pluck('nombre','id');
 
-    public function __construct(AreaInfluenciaRepository $areaInfluenciaRepo)
-    {
-        $this->areaInfluenciaRepository = $areaInfluenciaRepo;
+    if (empty($areaInfluencia)) {
+      Flash::error('Area Influencia not found');
+      return redirect(route('areaInfluencias.index'));
     }
 
-    /**
-     * Display a listing of the AreaInfluencia.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function index(Request $request)
-    {
-        $this->areaInfluenciaRepository->pushCriteria(new RequestCriteria($request));
-        $areaInfluencias = $this->areaInfluenciaRepository->all();
+    return view('area_influencias.edit')->with('areaInfluencia', $areaInfluencia)->with('condicionesdrenaje', $condicionesdrenaje)->with('permeabilidadsuelo', $permeabilidadsuelo)->with('usosuelo', $usosuelo)->with('caracteristicasetnicas', $caracteristicasetnicas)->with('tiposuelo', $tiposuelo)->with('clima', $clima)->with('ecosistema', $ecosistema)->with('manejoambiental', $manejoambiental);
 
-        return view('area_influencias.index')
-            ->with('areaInfluencias', $areaInfluencias);
+  }
+  /**
+  * Update the specified AreaInfluencia in storage.
+  *
+  * @param  int              $id
+  * @param UpdateAreaInfluenciaRequest $request
+  *
+  * @return Response
+  */
+  public function update($id, UpdateAreaInfluenciaRequest $request)
+  {
+    $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
+    if (empty($areaInfluencia)) {
+      Flash::error('Area Influencia not found');
+      return redirect(route('areaInfluencias.index'));
     }
-
-    /**
-     * Show the form for creating a new AreaInfluencia.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        return view('area_influencias.create');
+    $areaInfluencia = $this->areaInfluenciaRepository->update($request->all(), $id);
+    Flash::success('Area Influencia updated successfully.');
+    return redirect(route('areaInfluencias.index'));
+  }
+  /**
+  * Remove the specified AreaInfluencia from storage.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function destroy($id)
+  {
+    $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
+    if (empty($areaInfluencia)) {
+      Flash::error('Area Influencia not found');
+      return redirect(route('areaInfluencias.index'));
     }
-
-    /**
-     * Store a newly created AreaInfluencia in storage.
-     *
-     * @param CreateAreaInfluenciaRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreateAreaInfluenciaRequest $request)
-    {
-        $input = $request->all();
-
-        $areaInfluencia = $this->areaInfluenciaRepository->create($input);
-
-        Flash::success('Area Influencia saved successfully.');
-
-        return redirect(route('areaInfluencias.index'));
+    $this->areaInfluenciaRepository->delete($id);
+    Flash::success('Area Influencia deleted successfully.');
+    return redirect(route('areaInfluencias.index'));
+  }
+  public function storeTipoVegetal(Request $request, $idareainfluencia)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->tipoVegetals()->attach($request->TipoVegetal_id);
+    Flash::success('Areainfluencia  Has  Tipo Vegetal saved successfully.');
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function destroyTipoVegetal($idareainfluencia, $id)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->tipoVegetals()->detach($id);
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function storeReligion(Request $request, $idareainfluencia)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->religions()->attach($request->Religion_id);
+    Flash::success('Areainfluencia  Has  Religion saved successfully.');
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function destroyReligion($idareainfluencia, $id)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->religions()->detach($id);
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function storeLenguaje(Request $request, $idareainfluencia)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->lenguajes()->attach($request->Lenguaje_id);
+    Flash::success('Areainfluencia  Has  Lenguaje saved successfully.');
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function destroyLenguaje($idareainfluencia, $id)
+  {
+    $areainfluencia = AreaInfluencia::find($idareainfluencia);
+    $areainfluencia->lenguajes()->detach($id);
+    return redirect(url('areainfluencias/' . $areainfluencia->id));
+  }
+  public function areaInfluenciasHTMLPDF(Request $request)
+  {
+    $productos = $this->areaInfluenciaRepository->all();//OBTENGO TODOS MIS PRODUCTO
+    view()->share('areaInfluencias',$productos);//VARIABLE GLOBAL PRODUCTOS
+    if($request->has('descargar')){
+      $pdf = PDF::loadView('pdf.tablaInfluencias',compact('productos'));//CARGO LA VISTA
+      return $pdf->stream('areaInfluencias.pdf');//SUGERIR NOMBRE A DESCARGAR
     }
-
-    /**
-     * Display the specified AreaInfluencia.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function show($id)
-    {
-        $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
-
-        if (empty($areaInfluencia)) {
-            Flash::error('Area Influencia not found');
-
-            return redirect(route('areaInfluencias.index'));
-        }
-
-        return view('area_influencias.show')->with('areaInfluencia', $areaInfluencia);
-    }
-
-    /**
-     * Show the form for editing the specified AreaInfluencia.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
-
-        if (empty($areaInfluencia)) {
-            Flash::error('Area Influencia not found');
-
-            return redirect(route('areaInfluencias.index'));
-        }
-
-        return view('area_influencias.edit')->with('areaInfluencia', $areaInfluencia);
-    }
-
-    /**
-     * Update the specified AreaInfluencia in storage.
-     *
-     * @param  int              $id
-     * @param UpdateAreaInfluenciaRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdateAreaInfluenciaRequest $request)
-    {
-        $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
-
-        if (empty($areaInfluencia)) {
-            Flash::error('Area Influencia not found');
-
-            return redirect(route('areaInfluencias.index'));
-        }
-
-        $areaInfluencia = $this->areaInfluenciaRepository->update($request->all(), $id);
-
-        Flash::success('Area Influencia updated successfully.');
-
-        return redirect(route('areaInfluencias.index'));
-    }
-
-    /**
-     * Remove the specified AreaInfluencia from storage.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $areaInfluencia = $this->areaInfluenciaRepository->findWithoutFail($id);
-
-        if (empty($areaInfluencia)) {
-            Flash::error('Area Influencia not found');
-
-            return redirect(route('areaInfluencias.index'));
-        }
-
-        $this->areaInfluenciaRepository->delete($id);
-
-        Flash::success('Area Influencia deleted successfully.');
-
-        return redirect(route('areaInfluencias.index'));
-    }
-    public function areaInfluenciasHTMLPDF(Request $request)
-    {
-        $productos = $this->areaInfluenciaRepository->all();//OBTENGO TODOS MIS PRODUCTO
-        view()->share('areainfluencias',$productos);//VARIABLE GLOBAL PRODUCTOS
-        if($request->has('descargar')){
-            $pdf = PDF::loadView('pdf.tablaInfluencias',compact('productos'));//CARGO LA VISTA
-            return $pdf->stream('areaInfluencias.pdf');//SUGERIR NOMBRE A DESCARGAR
-        }
-        return view('areaInfluencias-pdf');//RETORNO A MI VISTA
-    }
+    return view('areaInfluencias-pdf');//RETORNO A MI VISTA
+  }
 }
--- app/Http/Controllers/DesechotController.php
+++ app/Http/Controllers/DesechotController.php
@@ -18,204 +18,194 @@ use Barryvdh\DomPDF\Facade as PDF;
 
 class DesechotController extends AppBaseController
 {
-    /** @var  DesechotRepository */
-    private $desechotRepository;
+  /** @var  DesechotRepository */
+  private $desechotRepository;
 
-    public function __construct(DesechotRepository $desechotRepo)
-    {
-        $this->desechotRepository = $desechotRepo;
+  public function __construct(DesechotRepository $desechotRepo)
+  {
+    $this->desechotRepository = $desechotRepo;
+  }
+
+  /**
+  * Display a listing of the Desechot.
+  *
+  * @param Request $request
+  * @return Response
+  */
+  public function index(Request $request)
+  {
+    $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
+    $this->desechotRepository->pushCriteria(new RequestCriteria($request));
+    $desechots = $this->desechotRepository->all();
+
+    return view('desechots.index')
+    ->with('desechots', $desechots)
+    ->with('tipodesechot', $tipodesechot)
+    ->with('chart',$this->createChart($desechots));
+  }
+
+  /**
+  * Show the form for creating a new Desechot.
+  *
+  * @return Response
+  */
+  public function create()
+  {
+    $taller       = Taller::all()->pluck('nombre', 'id');
+    $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
+    return view('desechots.create', [
+      'taller'       => $taller,
+      'tipodesechot' => $tipodesechot,
+
+    ]);
+  }
+
+  /**
+  * Store a newly created Desechot in storage.
+  *
+  * @param CreateDesechotRequest $request
+  *
+  * @return Response
+  */
+  public function store(CreateDesechotRequest $request)
+  {
+    $input = $request->all();
+
+    $desechot = $this->desechotRepository->create($input);
+
+    Flash::success('Guardado exitosamente.');
+
+    return redirect(route('desechots.index'));
+  }
+
+  /**
+  * Display the specified Desechot.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function show($id)
+  {
+    $taller       = Taller::all()->pluck('nombre', 'id');
+    $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
+    $desechot     = $this->desechotRepository->findWithoutFail($id);
+
+    if (empty($desechot)) {
+      Flash::error('Desechot not found');
+
+      return redirect(route('desechots.index'));
     }
 
-    /**
-     * Display a listing of the Desechot.
-     *
-     * @param Request $request
-     * @return Response
-     */
-    public function index(Request $request)
-    {
-        $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
-        $this->desechotRepository->pushCriteria(new RequestCriteria($request));
-        $desechots = $this->desechotRepository->all();
+    return view('desechots.show')->with('desechot', $desechot)->with('taller', $taller)->with('tipodesechot', $tipodesechot);
+  }
 
-        return view('desechots.index')
-            ->with('desechots', $desechots)
-            ->with('tipodesechot', $tipodesechot)
-            ->with('chart',$this->createChart($desechots));
+  /**
+  * Show the form for editing the specified Desechot.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function edit($id)
+  {
+    $taller       = Taller::all()->pluck('nombre', 'id');
+    $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
+    $desechot     = $this->desechotRepository->findWithoutFail($id);
+
+    if (empty($desechot)) {
+      Flash::error('No se ha encontrado');
+
+      return redirect(route('desechots.index'));
     }
 
-    /**
-     * Show the form for creating a new Desechot.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        $taller       = Taller::all()->pluck('nombre', 'id');
-        $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
-        return view('desechots.create', [
-            'taller'       => $taller,
-            'tipodesechot' => $tipodesechot,
+    return view('desechots.edit')->with('desechot', $desechot)->with('taller', $taller)->with('tipodesechot', $tipodesechot);
+  }
 
-        ]);
+  /**
+  * Update the specified Desechot in storage.
+  *
+  * @param  int              $id
+  * @param UpdateDesechotRequest $request
+  *
+  * @return Response
+  */
+  public function update($id, UpdateDesechotRequest $request)
+  {
+    $desechot = $this->desechotRepository->findWithoutFail($id);
+
+    if (empty($desechot)) {
+      Flash::error('No se ha encontrado');
+
+      return redirect(route('desechots.index'));
     }
 
-    /**
-     * Store a newly created Desechot in storage.
-     *
-     * @param CreateDesechotRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreateDesechotRequest $request)
-    {
-        $input = $request->all();
+    $desechot = $this->desechotRepository->update($request->all(), $id);
 
-        $desechot = $this->desechotRepository->create($input);
+    Flash::success('Actualizado exitosamente.');
 
-        Flash::success('Guardado exitosamente.');
+    return redirect(route('desechots.index'));
+  }
 
-        return redirect(route('desechots.index'));
+  /**
+  * Remove the specified Desechot from storage.
+  *
+  * @param  int $id
+  *
+  * @return Response
+  */
+  public function destroy($id)
+  {
+    $desechot = $this->desechotRepository->findWithoutFail($id);
+
+    if (empty($desechot)) {
+      Flash::error('No se ha encontrado');
+
+      return redirect(route('desechots.index'));
     }
 
-    /**
-     * Display the specified Desechot.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function show($id)
-    {
-        $taller       = Taller::all()->pluck('nombre', 'id');
-        $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
-        $desechot     = $this->desechotRepository->findWithoutFail($id);
+    $this->desechotRepository->delete($id);
 
-        if (empty($desechot)) {
-            Flash::error('Desechot not found');
+    Flash::success('Eliminado exitosamente.');
 
-            return redirect(route('desechots.index'));
-        }
+    return redirect(route('desechots.index'));
+  }
 
-        return view('desechots.show')->with('desechot', $desechot)->with('taller', $taller)->with('tipodesechot', $tipodesechot);
+
+  public function createChart($desechos) {
+    $preprocessedDataset = $desechos->sortBy('fecha');
+    $preprocessedDataset = $preprocessedDataset->filter(function ($item) {
+      return $item->fecha->diffInMonths(Carbon::now()) <= 12;
+    });
+
+    $dataset = collect();
+    foreach ($preprocessedDataset as $desecho) {
+      $temp = [
+        'peso' => (float)$desecho->peso,
+        'fecha' => Carbon::parse($desecho->fecha)->format('M-Y') ,
+        'taller' => $desecho->taller->nombre
+      ];
+      $dataset->push($temp);
     }
+    $dataset = $dataset->groupBy('taller');
+    $dataset = $dataset->map(function ($item) {
+      return $item->groupBy('fecha')->map(function ($item2){
+        return $item2->sum('peso');
+      });
+    });
+    $labels = $dataset->collapse()->toArray();
+    $labels = array_fill_keys(array_keys($labels), 0);
 
-    /**
-     * Show the form for editing the specified Desechot.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        $taller       = Taller::all()->pluck('nombre', 'id');
-        $tipodesechot = TipoDesechot::all()->pluck('nombre', 'id');
-        $desechot     = $this->desechotRepository->findWithoutFail($id);
+    $dataset = $dataset->toArray();
 
-        if (empty($desechot)) {
-            Flash::error('No se ha encontrado');
-
-            return redirect(route('desechots.index'));
-        }
-
-        return view('desechots.edit')->with('desechot', $desechot)->with('taller', $taller)->with('tipodesechot', $tipodesechot);
+    $chart = new DefaultChart;
+    foreach ($dataset as $key => $item) {
+      $chart->dataset($key, 'column', array_values(array_merge($labels,$item)));
     }
+    $chart->labels(array_keys($labels));
+    $chart->title('Total de Desechos Generados por Taller en los Últimos 12 Meses');
+    $chart->label("Cantidad de Desechos (Kg)");
+    return $chart;
 
-    /**
-     * Update the specified Desechot in storage.
-     *
-     * @param  int              $id
-     * @param UpdateDesechotRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdateDesechotRequest $request)
-    {
-        $desechot = $this->desechotRepository->findWithoutFail($id);
-
-        if (empty($desechot)) {
-            Flash::error('No se ha encontrado');
-
-            return redirect(route('desechots.index'));
-        }
-
-        $desechot = $this->desechotRepository->update($request->all(), $id);
-
-        Flash::success('Actualizado exitosamente.');
-
-        return redirect(route('desechots.index'));
-    }
-
-    /**
-     * Remove the specified Desechot from storage.
-     *
-     * @param  int $id
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $desechot = $this->desechotRepository->findWithoutFail($id);
-
-        if (empty($desechot)) {
-            Flash::error('No se ha encontrado');
-
-            return redirect(route('desechots.index'));
-        }
-
-        $this->desechotRepository->delete($id);
-
-        Flash::success('Eliminado exitosamente.');
-
-        return redirect(route('desechots.index'));
-    }
-<<<<<<< HEAD
-
-    public function createChart($desechos) {
-        $preprocessedDataset = $desechos->sortBy('fecha');
-        $preprocessedDataset = $preprocessedDataset->filter(function ($item) {
-            return $item->fecha->diffInMonths(Carbon::now()) <= 12;
-        });
-
-        $dataset = collect();
-        foreach ($preprocessedDataset as $desecho) {
-            $temp = [
-                'peso' => (float)$desecho->peso,
-                'fecha' => Carbon::parse($desecho->fecha)->format('M-Y') ,
-                'taller' => $desecho->taller->nombre
-            ];
-            $dataset->push($temp);
-        }
-        $dataset = $dataset->groupBy('taller');
-        $dataset = $dataset->map(function ($item) {
-            return $item->groupBy('fecha')->map(function ($item2){
-                return $item2->sum('peso');
-            });
-        });
-        $labels = $dataset->collapse()->toArray();
-        $labels = array_fill_keys(array_keys($labels), 0);
-
-        $dataset = $dataset->toArray();
-
-        $chart = new DefaultChart;
-        foreach ($dataset as $key => $item) {
-            $chart->dataset($key, 'column', array_values(array_merge($labels,$item)));
-        }
-        $chart->labels(array_keys($labels));
-        $chart->title('Total de Desechos Generados por Taller en los Últimos 12 Meses');
-        $chart->label("Cantidad de Desechos (Kg)");
-        return $chart;
-=======
-    public function desechotsHTMLPDF(Request $request)
-    {
-        $productos = $this->desechotRepository->all();//OBTENGO TODOS MIS PRODUCTO
-        view()->share('desechots',$productos);//VARIABLE GLOBAL PRODUCTOS
-        if($request->has('descargar')){
-            $pdf = PDF::loadView('pdf.tablaDesechosT',compact('productos'));//CARGO LA VISTA
-            return $pdf->stream('DesechosT.pdf');//SUGERIR NOMBRE A DESCARGAR
-        }
-        return view('desechots-pdf');//RETORNO A MI VISTA
->>>>>>> master
-    }
+  }
 }
